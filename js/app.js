99420c6e633acce832fbbbe06abb51bc6bf03983
Stefano.In91 <ste.ince91@gmail.com>
2022-11-16 16:42:31 +0100

    adds email api to created hook

--- js/app.js
+++ js/app.js
@@ -6,5 +6,13 @@ createApp({
       mailList: [],
     };
   },
-  created() {},
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+        .then((response) => {
+          this.mailList.push(response.data.response);
+        });
+    }
+  },
 }).mount("#app");
